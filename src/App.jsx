4874e060bb468d11c0b3e9e1b59f546991d4c283
Raghavcommercialwinner <iraghavkaushik@gmail.com>
2025-05-26 21:30:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Sidebarnew from '../components/Sidebarnew'
 import Chatbox from '../components/Chatbox'
-
+import Sidebar from '../components/Sidebar'
 const App = () => {
   return (
 
     <div className='h-screen bg-black mt-0 flex gap-1'> 
         <Sidebarnew />
-        <Chatbox />        
+        <Chatbox />
+        <Sidebar />
     </div>
     
   )
